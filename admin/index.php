f19e86d665990cbf15ceafaaf63ae905f7494567
Tristan White <oliver.tristan@gmail.com>
2016-07-02 16:48:52 +0200

    Add SQLite db file

--- admin/index.php
+++ admin/index.php
@@ -1,2 +1,32 @@
 <?php
-echo "it works!";
+
+
+
+
+
+class MyDB extends SQLite3 {
+  function __construct() {
+    $this->open('../colors.db');
+  }
+}
+$db = new MyDB();
+if(!$db){
+  echo $db->lastErrorMsg();
+} else {
+  echo "Opened database successfully<br>";
+}
+
+$sql =<<<EOF
+SELECT * from COLORS;
+EOF;
+
+$ret = $db->query($sql);
+while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+  echo "ID = ". $row['id'] . "<br>";
+  echo "HEX = ". $row['hex'] ."<br>";
+  echo "RGB = ". $row['rgb'] ."<br>";
+  echo "GROUP = ".$row['color_grp'] ."<br><br>";
+}
+echo "Operation done successfully<br>";
+
+$db->close();
