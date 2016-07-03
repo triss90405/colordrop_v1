0d8304e2892af2abf65fbb9d86ba202ec13d6c71
Tristan White <oliver.tristan@gmail.com>
2016-07-03 16:25:24 +0200

    Add proper admin favicon files

--- faYfe2PCsQGvRfaeIyjs/index.php
+++ faYfe2PCsQGvRfaeIyjs/index.php
@@ -6,12 +6,24 @@
   <meta name="description" content="Admin page">
   <meta name="author" content="Tristan White">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/admin/apple-touch-icon.png">
+  <link rel="stylesheet" href="/assets/css/main.css" charset="utf-8">
+  <link rel="apple-touch-icon" sizes="57x57" href="/assets/img/admin/apple-touch-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="/assets/img/admin/apple-touch-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="/assets/img/admin/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="/assets/img/admin/apple-touch-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="/assets/img/admin/apple-touch-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="/assets/img/admin/apple-touch-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="/assets/img/admin/apple-touch-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="/assets/img/admin/apple-touch-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/admin/apple-touch-icon-180x180.png">
   <link rel="icon" type="image/png" href="/assets/img/admin/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="/assets/img/admin/android-chrome-192x192.png" sizes="192x192">
   <link rel="icon" type="image/png" href="/assets/img/admin/favicon-16x16.png" sizes="16x16">
   <link rel="manifest" href="/assets/img/admin/manifest.json">
-  <link rel="mask-icon" href="/assets/img/admin/safari-pinned-tab.svg" color="#5bbad5">
+  <link rel="mask-icon" href="/assets/img/admin/safari-pinned-tab.svg" color="#33accf">
   <link rel="shortcut icon" href="/assets/img/admin/favicon.ico">
+  <meta name="msapplication-TileColor" content="#333333">
+  <meta name="msapplication-TileImage" content="/assets/img/admin/mstile-144x144.png">
   <meta name="msapplication-config" content="/assets/img/admin/browserconfig.xml">
   <meta name="theme-color" content="#ffffff">
 </head>
