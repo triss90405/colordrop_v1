60ed96206becb7f745bc0cda864a69df5f1e1835
Tristan White <oliver.tristan@gmail.com>
2016-07-14 14:42:22 +0200

    Add new font - Proxima Nova Soft

--- assets/js/functions/functions.js
+++ assets/js/functions/functions.js
@@ -197,6 +197,66 @@ function declineCookies() {
 }
 
 
+//
+// function addFavourite() {
+//   $('.sidebar__favourite--add').on('click', function(e){
+//     e.preventDefault();
+//     var favourite_add = $(this).val(),
+//         currentButton = $(this);
+//     // Returns successful data submission message when the entered information is stored in database.
+//     var dataString = 'id=' + favourite_add;
+//     if (favourite_add === '') {
+//     } else {
+//       // AJAX code to submit form.
+//       $.ajax({
+//         type: "POST",
+//         url: "favourite_add_post.php",
+//         data: dataString,
+//         cache: false,
+//         success: function() {
+//           currentButton.addClass('active');
+//           currentButton.children(':last').text('Remove');
+//           currentButton.parent().parent().parent().prev().append('<svg id="favourite" baseProfile="tiny" height="24px" id="Layer_1" version="1.2" viewBox="0 0 24 24" width="24px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><filter xmlns="http://www.w3.org/2000/svg" id="dropshadow" height="130%"><feGaussianBlur in="SourceAlpha" stdDeviation="1"/><feOffset dx="1" dy="1" result="offsetblur"/><feComponentTransfer><feFuncA type="linear" slope="0.6"/></feComponentTransfer><feMerge><feMergeNode/><feMergeNode in="SourceGraphic"/></feMerge></filter></defs><path filter="url(#dropshadow)" d="M9.362,9.158c0,0-3.16,0.35-5.268,0.584c-0.19,0.023-0.358,0.15-0.421,0.343s0,0.394,0.14,0.521    c1.566,1.429,3.919,3.569,3.919,3.569c-0.002,0-0.646,3.113-1.074,5.19c-0.036,0.188,0.032,0.387,0.196,0.506    c0.163,0.119,0.373,0.121,0.538,0.028c1.844-1.048,4.606-2.624,4.606-2.624s2.763,1.576,4.604,2.625    c0.168,0.092,0.378,0.09,0.541-0.029c0.164-0.119,0.232-0.318,0.195-0.505c-0.428-2.078-1.071-5.191-1.071-5.191    s2.353-2.14,3.919-3.566c0.14-0.131,0.202-0.332,0.14-0.524s-0.23-0.319-0.42-0.341c-2.108-0.236-5.269-0.586-5.269-0.586    s-1.31-2.898-2.183-4.83c-0.082-0.173-0.254-0.294-0.456-0.294s-0.375,0.122-0.453,0.294C10.671,6.26,9.362,9.158,9.362,9.158z"/></svg>');
+//           currentButton.addClass('sidebar__favourite--remove');
+//           currentButton.removeClass('sidebar__favourite--add');
+//           currentButton.removeClass('active');
+//         }
+//       });
+//     }
+//     return false;
+//   });
+// }
+
+//
+// function removeFavourite() {
+//   $('.sidebar__favourite--remove').on('click', function(e){
+//     e.preventDefault();
+//     var favourite_remove = $(this).val(),
+//         currentButton = $(this);
+//     // Returns successful data submission message when the entered information is stored in database.
+//     var dataString = 'id=' + favourite_remove;
+//     if (favourite_remove === '') {
+//     } else {
+//       // AJAX code to submit form.
+//       $.ajax({
+//         type: "POST",
+//         url: "favourite_delete_post.php",
+//         data: dataString,
+//         cache: false,
+//         success: function() {
+//           currentButton.removeClass('active');
+//           currentButton.children(':last').text('Favourite');
+//           currentButton.parent().parent().parent().prev().remove('#favourite');
+//           currentButton.addClass('sidebar__favourite--add');
+//           currentButton.removeClass('sidebar__favourite--remove');
+//         }
+//       });
+//     }
+//     return false;
+//   });
+// }
+
+
 function toggleMenu() {
   var toggleButton = $('.navigation__toggle__button'),
       navigation = $('.navigation');
@@ -214,4 +274,6 @@ $(document).ready(function() {
   acceptCookies();
   declineCookies();
   toggleMenu();
+  // addFavourite();
+  // removeFavourite();
 });
