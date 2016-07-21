cf1d99a427ee8725debc6eef64124acea55db863
Tristan White <oliver.tristan@gmail.com>
2016-07-21 22:39:04 +0200

    Temporarily disable gridder ad

--- inc/footer.php
+++ inc/footer.php
@@ -1,12 +1,12 @@
     <footer>
 
-    <div class="gridder_link">
+    <!-- <div class="gridder_link">
       <svg enable-background="new 0 0 32 32" height="32px" class="gridder_link_close" id="close" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
         <path d="M17.459,16.014l8.239-8.194c0.395-0.391,0.395-1.024,0-1.414c-0.394-0.391-1.034-0.391-1.428,0  l-8.232,8.187L7.73,6.284c-0.394-0.395-1.034-0.395-1.428,0c-0.394,0.396-0.394,1.037,0,1.432l8.302,8.303l-8.332,8.286  c-0.394,0.391-0.394,1.024,0,1.414c0.394,0.391,1.034,0.391,1.428,0l8.325-8.279l8.275,8.276c0.394,0.395,1.034,0.395,1.428,0  c0.394-0.396,0.394-1.037,0-1.432L17.459,16.014z" fill="#121313" id="Close"></path>
       </svg>
       <p>Have you tried <a href="https://gridder.io" target="_blank">gridder.io</a>?<br>
       It's a css flexbox grid </p>
-    </div>
+    </div> -->
 
     <script src="/assets/js/functions/min/main-min.js"></script>
 
